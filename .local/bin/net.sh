654d251f471b85808ef0721f06f45fc9f00de22a
amrit-44404 <amrit44404@proton.me>
2024-09-01 16:03:00 +0545

    new more bugfree config for disabling wifi_powersave

--- .local/bin/net.sh
+++ .local/bin/net.sh
@@ -1,6 +1,25 @@
+# Start Network Manager Service and Removing unnecessary stuffs
 sudo rm -rf /var/service/NetworkManager
 sudo rm -rf /var/service/dbus
 sudo rm -rf /var/service/wpa_supplicant
 sudo rm -rf /var/service/dhcpcd
+sudo rm -rf /var/service/agetty-tty3
+sudo rm -rf /var/service/agetty-tty4
+sudo rm -rf /var/service/agetty-tty5
+sudo rm -rf /var/service/agetty-tty6
 sudo ln -s /etc/sv/NetworkManager/ /var/service/
 sudo ln -s /etc/sv/dbus/ /var/service/
+
+# Disabling Wifi_Powersave
+sudo rm -rf /etc/NetworkManager/conf.d
+sudo mkdir -p /etc/NetworkManager/conf.d
+sudo touch /etc/NetworkManager/conf.d/wifi-powersave.conf
+LOC="/etc/NetworkManager/conf.d/wifi-powersave.conf"
+	echo "The following has been added to $LOC.\n"
+	echo "[connection]\nwifi.powersave = 2" | sudo tee $LOC > /dev/null
+	echo "The following has been added to $LOC."
+	cat $LOC
+	echo
+	echo "Restarting NetworkManager service..."
+	sudo sv down NetworkManager
+	sudo sv up NetworkManager
