fee972b45feff2c266af8b30d22acc3ecb33007b
amrit-44404 <amrit44404@proton.me>
2024-09-01 18:38:04 +0545

    some minor changes

--- .local/bin/net.sh
+++ .local/bin/net.sh
@@ -22,7 +22,7 @@ LOC="/etc/NetworkManager/conf.d/wifi-powersave.conf"
 	echo "The following has been added to $LOC."
 	cat $LOC
 	echo
+	sleep 3
 	echo "Restarting NetworkManager service..."
 	# For Runit Systems
-	sudo sv down NetworkManager
-	sudo sv up NetworkManager
+	sudo sv restart NetworkManager
