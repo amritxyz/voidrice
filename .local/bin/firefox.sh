337c0225dfaa037abf27a5147e43a9b21942efab
nyx-void <amrit44404@proton.me>
2024-12-17 12:49:35 +0545

    just the indententations

--- .local/bin/firefox.sh
+++ .local/bin/firefox.sh
@@ -6,8 +6,8 @@ profilesini="$browserdir/profiles.ini"
 
 # Start firefox in headless mode
 if [ ! -f "$profilesini" ]; then
-    sudo -u "$USER" firefox --headless >/dev/null 2>&1 &
-    sleep 1
+	sudo -u "$USER" firefox --headless >/dev/null 2>&1 &
+	sleep 1
 fi
 
 # Extract profile directory
@@ -16,19 +16,19 @@ pdir="$browserdir/$profile"
 
 # Ensure profile directory exists
 if [ -d "$pdir" ]; then
-    # Custom user.js
-    custom_userjs="/home/$USER/.local/share/user.js"
+	# Custom user.js
+	custom_userjs="/home/$USER/.local/share/user.js"
 
     # Check if custom user.js exists
     if [ -f "$custom_userjs" ]; then
-        # Install the custom user.js to the Firefox profile
-	rm -rf $pdir/* && rm -rf $pdir/.parentlock
-        cp "$custom_userjs" "$pdir/user.js"
-	killall firefox
-        echo "Custom user.js has been installed to the profile."
+	    # Install the custom user.js to the Firefox profile
+	    rm -rf $pdir/* && rm -rf $pdir/.parentlock
+	    cp "$custom_userjs" "$pdir/user.js"
+	    killall firefox
+	    echo "Custom user.js has been installed to the profile."
     else
-        echo "Error: ~/.local/share/user.js not found."
+	    echo "Error: ~/.local/share/user.js not found."
     fi
 else
-    echo "Error: Firefox profile directory not found."
+	echo "Error: Firefox profile directory not found."
 fi
--- .local/bin/scrot.sh
+++ .local/bin/scrot.sh
@@ -5,19 +5,19 @@ TARGET_DIR="$HOME/.local/ss"
 
 # Check if the directory exists
 if [ ! -d "$TARGET_DIR" ]; then
-    mkdir -p "$TARGET_DIR"
+	mkdir -p "$TARGET_DIR"
 fi
 
 # Find the highest number used in existing filenames
 MAX_NUMBER=$(ls -1 "$TARGET_DIR"/*_screenshot.png 2>/dev/null | \
-              awk -F'[_]' '{print $1}' | \
-              awk -F/ '{print $NF}' | \
-              sort -n | \
-              tail -1)
+	awk -F'[_]' '{print $1}' | \
+	awk -F/ '{print $NF}' | \
+	sort -n | \
+	tail -1)
 
 # Check if no files were found
 if [ -z "$MAX_NUMBER" ]; then
-  MAX_NUMBER=0
+	MAX_NUMBER=0
 fi
 
 # Determine the next number
