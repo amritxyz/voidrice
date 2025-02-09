2a990bc224c0042cf048e0f79adb7d0d18520d54
amritxyz <amrit44404@proton.me>
2025-02-09 15:49:15 +0545

    changes from bsd

--- .local/bin/firefox.sh
+++ .local/bin/firefox.sh
@@ -2,22 +2,18 @@
 # Define Firefox directory and profile location
 browserdir="/home/void/.mozilla/firefox"
 profilesini="$browserdir/profiles.ini"
-
 # Start firefox in headless mode
 if [ ! -f "$profilesini" ]; then
 	sudo -u "$USER" firefox --headless >/dev/null 2>&1 &
 	sleep 1
 fi
-
 # Extract profile directory
 profile=$(sed -n "/Default=.*.default/ s/.*=//p" "$profilesini")
 pdir="$browserdir/$profile"
-
 # Ensure profile directory exists
 if [ -d "$pdir" ]; then
 	# Custom user.js
 	custom_userjs="/home/$USER/.local/share/arkenfox.js"
-
     # Check if custom user.js exists
     if [ -f "$custom_userjs" ]; then
 	    # Install the custom user.js to the Firefox profile
